6c44b056f7ac7490dc77762f7bf6ab2361a2696b
Yuki Suga <ysuga@ysuga.net>
2014-09-05 13:02:07 +0900

    This if first commit. This repository is generated by wasanbon

---
+++ src/Localization_MRPT.cpp
@@ -0,0 +1,176 @@
+// -*- C++ -*-
+/*!
+ * @file  Localization_MRPT.cpp
+ * @brief Localization MRPT Component
+ * @date $Date$
+ *
+ * $Id$
+ */
+
+#include "Localization_MRPT.h"
+
+// Module specification
+// <rtc-template block="module_spec">
+static const char* localization_mrpt_spec[] =
+  {
+    "implementation_id", "Localization_MRPT",
+    "type_name",         "Localization_MRPT",
+    "description",       "Localization MRPT Component",
+    "version",           "1.0.0",
+    "vendor",            "Sugar Sweet Robotics",
+    "category",          "Navigatio",
+    "activity_type",     "PERIODIC",
+    "kind",              "DataFlowComponent",
+    "max_instance",      "1",
+    "language",          "C++",
+    "lang_type",         "compile",
+    // Configuration variables
+    "conf.default.debug", "0",
+    // Widget
+    "conf.__widget__.debug", "text",
+    // Constraints
+    ""
+  };
+// </rtc-template>
+
+/*!
+ * @brief constructor
+ * @param manager Maneger Object
+ */
+Localization_MRPT::Localization_MRPT(RTC::Manager* manager)
+    // <rtc-template block="initializer">
+  : RTC::DataFlowComponentBase(manager),
+    m_rangeIn("range", m_range),
+    m_odometryIn("odometry", m_odometry),
+    m_estimatedPoseOut("estimatedPose", m_estimatedPose)
+
+    // </rtc-template>
+{
+}
+
+/*!
+ * @brief destructor
+ */
+Localization_MRPT::~Localization_MRPT()
+{
+}
+
+
+
+RTC::ReturnCode_t Localization_MRPT::onInitialize()
+{
+  // Registration: InPort/OutPort/Service
+  // <rtc-template block="registration">
+  // Set InPort buffers
+  addInPort("range", m_rangeIn);
+  addInPort("odometry", m_odometryIn);
+  
+  // Set OutPort buffer
+  addOutPort("estimatedPose", m_estimatedPoseOut);
+  
+  // Set service provider to Ports
+  
+  // Set service consumers to Ports
+  
+  // Set CORBA Service Ports
+  
+  // </rtc-template>
+
+  // <rtc-template block="bind_config">
+  // Bind variables and configuration variable
+  bindParameter("debug", m_debug, "0");
+  // </rtc-template>
+  
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t Localization_MRPT::onFinalize()
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t Localization_MRPT::onStartup(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t Localization_MRPT::onShutdown(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+RTC::ReturnCode_t Localization_MRPT::onActivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t Localization_MRPT::onDeactivated(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+
+RTC::ReturnCode_t Localization_MRPT::onExecute(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+
+/*
+RTC::ReturnCode_t Localization_MRPT::onAborting(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t Localization_MRPT::onError(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t Localization_MRPT::onReset(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t Localization_MRPT::onStateUpdate(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+/*
+RTC::ReturnCode_t Localization_MRPT::onRateChanged(RTC::UniqueId ec_id)
+{
+  return RTC::RTC_OK;
+}
+*/
+
+
+
+extern "C"
+{
+ 
+  void Localization_MRPTInit(RTC::Manager* manager)
+  {
+    coil::Properties profile(localization_mrpt_spec);
+    manager->registerFactory(profile,
+                             RTC::Create<Localization_MRPT>,
+                             RTC::Delete<Localization_MRPT>);
+  }
+  
+};
+
+
